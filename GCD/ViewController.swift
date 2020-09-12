31b1ca25debb47fdc781c40b2ac96e45310f0743
Andrey <KonkeyDong37@gmail.com>
2020-09-12 16:46:23 +0500

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Андрей on 12.09.2020.
+//  Copyright © 2020 Андрей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
